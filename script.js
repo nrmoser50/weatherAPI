627ecf7027794580e1f5666a91cca9274f92a271
Nick Moser <115385996+nrmoser50@users.noreply.github.com>
2022-12-16 16:13:42 -0700

    Updated API calls

--- script.js
+++ script.js
@@ -1,45 +1,65 @@
 var APIkey = "57984c092f03848689bc2922b00b4d3e";
-var weatherForecastEl = document.getElementById("#forecast");
+// var weatherForecastEl = document.getElementById("#forecast");
 var lastCitySearched;
-var storedCities;
-var forecastSearch = document.getElementById("#city-info");
+var storedCities = []
+// var forecastSearch = document.getElementById("#city-info");
 var currentDate = dayjs().format();
 var input = $(".form-control")
-var cities;
+var user = document.getElementById("#city-input")
 
+init ();
+
+// Get previously stored cities
+function init () {
+	localStorage.getItem("Cities") 
+		storedCities = JSON.parse(localStorage.getItem("Cities"));
+		console.log(storedCities);
+		// for (var i = 0; i < storedCities.length; i++) {
+		// 	lastCitySearched = storedCities.length - 1;
+		// 	var lastCity = storedCities[lastCitySearched];
+//}
+};
 
 // 
-// Get previously stored cities
-if (localStorage.getItem("cities")) {
-	storedCities = JSON.parse(localStorage.getItem("cities"));
-	console.log(storedCities);
-	for (var i = 0; i < storedCities.length; i++) {
-		lastCitySearched = storedCities.length - 1;
-		var lastCity = storedCities[lastCitySearched];
-	}
-} else {
-	cities;
-}
 
-window.localStorage.setItem("Cities", JSON.stringify(lastCity))
+
 
 // search for cities
 $("#search-city").on("click", function (event) {
     event.preventDefault();
     // get city input
     var city = $("#city-input").val();
-    console.log(city); 
-	fetchAPI()
+    console.log(city);
+	getCoordinates(city); 
+	
+	localStorage.setItem("Cities", JSON.stringify(city))
 
 });
 
-function fetchAPI () {
-	fetch ('https://api.openweathermap.org/data/2.5/forecast?q=' + input.val() + '&appid=' + APIkey)
+function getCoordinates (cityName) {
+	console.log(cityName);
+	fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIkey}&units=imperial`)
+	.then(function (response) {
+		if (response.ok) {
+			return response.json().then(function (data) {
+				// console.log(data)
+				console.log(data[0].lat)
+				console.log(data[0].lon)
+				fetchAPI(data[0].lat, data[0].lon);
+			})
+		}
+		
+	})
+	} 
+	
+function fetchAPI (lat, lon) {
+	fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`)
 	.then (function (response) {
 		if (response.ok) {
 			return response.json().then(function (data) {
 				console.log(data)
 				renderForecast(data);
+				// renderIcons();
 				
 				// renderCityName();
 			});
@@ -51,20 +71,17 @@ function fetchAPI () {
 
 
 function renderForecast(response) {
-	console.log(response.list[0].main.temp);
 	
 	$("#temperature").text(response.list[0].main.temp);
 	$("#humidity").text(response.list[0].main.humidity);
 	$("#wind-speed").text(response.list[0].wind.speed);
-	
-	
+	// for (let i = 6; i < list.length; i++) 
 }
 
-// function renderCityName(response)
-// 	$(".card-title").text(currentDate)
-// 	var weatherIcon = $("<img>");
-// 	var iconCode = a.weather[0].icon
-// 	var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
-// 	weatherIcon.attr("src", iconUrl);
-// 	$(".card-title").attr('src', iconUrl);
+function renderIcons(response) {
+var iconCode = a.weather[0].icon;
+console.log(iconCode);
+var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+$('#forecast').attr('src', iconURL);
+}
 
