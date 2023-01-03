71ac28d678f4cefa16bb5d34e422bddb234f5bd4
Nick Moser <115385996+nrmoser50@users.noreply.github.com>
2023-01-03 09:13:11 -0700

    Added formatted dates and icons to forecasts.

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 const APIkey = "57984c092f03848689bc2922b00b4d3e";
-// var weatherForecastEl = document.getElementById("#forecast");
-var lastCitySearched;
-var storedCities = JSON.parse(localStorage.getItem("Cities")) || []
-// var forecastSearch = document.getElem= JSON.parse(localStorage.getItem("Cities"))entById("#city-info");
-let currentDate = new Date();
-let input = $(".form-control")
-var user = document.getElementById("#city-input");
+let storedCities = JSON.parse(localStorage.getItem("Cities")) || []
+let user = document.getElementById("#city-input");
+let forecastContainer = document.querySelector(".forecast-container");
+let currentContainer = document.querySelector(".card-body");
 
-let timeData = []
-// let forecastDiv = $(".forecast")
-var forecastContainer = document.querySelector(".forecast-container")
-
-
-
-// Get previously stored cities
+// Get previously stored cities on page load
 function init () { 
-		console.log(storedCities)
-		var ulEl = document.querySelector(".list-group");
+		
+		let ulEl = document.querySelector(".list-group");
 		ulEl.innerHTML = ""
 		for (var i = 0; i < storedCities.length; i++) {
-		var liEl = document.createElement("li");
+		let liEl = document.createElement("li");
 		liEl.setAttribute("class", "list-group-item")
 		liEl.textContent = storedCities[i]
 		liEl.setAttribute("data-cityName", storedCities[i])
@@ -27,39 +18,30 @@ function init () {
 			var cityName = $(this).attr("data-cityName")
 			getCoordinates(cityName);
 		}
-		ulEl.append(liEl)	
+		ulEl.append(liEl)
+		// console.log(storedCities)	
 		} 		
 }; 
-// display previously searched cities
-
 
 // search for cities
 $("#search-city").on("click", function (event) {
     event.preventDefault();
-    // get city input
-    var city = $("#city-input").val()
+    let city = $("#city-input").val()
     if (!storedCities.includes(city)) {
 		storedCities.push(city)
 		localStorage.setItem("Cities", JSON.stringify(storedCities))
 	}
 	getCoordinates(city); 
 	
-	
-	// window.localStorage.removeItem(city);
-	
-	
-	
-	
 	});
 
 function getCoordinates (cityName) {
 	init();
-	console.log(cityName);
+	// console.log(cityName);
 	fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIkey}&units=imperial`)
 	.then(function (response) {
 		if (response.ok) {
 			return response.json().then(function (data) {
-				// console.log(data)
 				// console.log(data[0].lat)
 				// console.log(data[0].lon)
 				fetchAPI(data[0].lat, data[0].lon);
@@ -74,88 +56,107 @@ function fetchAPI (lat, lon) {
 	.then (function (response) {
 		if (response.ok) {
 			return response.json().then(function (data) {
-				// console.log(data.list)
-				timeData = []
-				let weatherData = []
-				for (i = 0; i < data.list.length; i++){
+				
+				let weatherData = [];
+
+				for (var i = 0; i < data.list.length; i++){
 					// console.log(dayjs(data.list[i].dt_txt).hour());
 					if (12 === dayjs(data.list[i].dt_txt).hour()) {
-						// console.log("strings")
+
 						weatherData.push(data.list[i]);
 					}
 				}
-				console.log(data);
+				// console.log(data);
 				
 				renderForecast(weatherData);
-				// renderIcons();
 				
-				// renderCurrentSearch();
 			});
 		} else {
+
 			alert('Error: Unable to view this city. Try again.');
+
 		}}
 		)
+
 		fetchCurrentWeather(lat, lon)
 	}
 
 function fetchCurrentWeather(lat, lon) {
-	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`)
+	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`)
 	.then (function (response) {
 		if (response.ok) {
-			return response.json().then(function (data){
-				console.log(data)
+			return response.json().then(function (data) {
+				renderCurrent(data)
 			})
-		}})
+		}}); 
 
+}
+
+function renderCurrent(currentConditions) {
+	// console.log(currentConditions);
+	currentContainer.innerHTML = ""
+	var cityTitle = document.createElement("h3")
+	cityTitle.textContent = currentConditions.name
+	var imgEl = document.createElement("img")
+	imgEl.setAttribute("src", `http://openweathermap.org/img/wn/${currentConditions.weather[0].icon}@2x.png`)
+	cityTitle.appendChild(imgEl);
+	currentContainer.append(cityTitle);
+	var tempEl = document.createElement("p")
+	tempEl.textContent = "Temperature: " + Math.round(currentConditions.main.temp) + " F"
+	currentContainer.append(tempEl);
+	var windEl = document.createElement("p")
+	windEl.textContent = "Wind Speed: " + currentConditions.wind.speed + " mph"
+	currentContainer.append(windEl)
+	var humidityEl = document.createElement("p")
+	humidityEl.textContent = "Humidity: " + currentConditions.main.humidity + "%"
+	currentContainer.append(humidityEl);
+
+	
 }
 
 
 
 function renderForecast(weatherData) {
-	console.log(weatherData)
+	// console.log(weatherData)
 	forecastContainer.innerHTML = ""
 	
 	
 	for (var i = 0; i < weatherData.length; i++) {
+		var date = weatherData[i].dt_txt.split(" ")[0]
+		// console.log(date);
+		var year = date.split("-")[0];
+		var month = date.split("-")[1];
+		var day = date.split("-")[2];
+		var formattedDate = new Date(month + "/" + day + "/" + year).toString().split(" ")[0]
+		// console.log(formattedDate);
 		var cardDiv = document.createElement("div")
 		cardDiv.setAttribute("class", "forecast card")
-		
+		var cardTitle = document.createElement("h5")
+		cardTitle.textContent = formattedDate
 		var pTemp = document.createElement("p")
 		pTemp.setAttribute("class", "card-text temperature")
-		pTemp.textContent = "Temperature = " + weatherData[i].main.temp + " F";
+		pTemp.textContent = "Temperature: " + weatherData[i].main.temp + " F";
 		
 		var pHumidity = document.createElement("p")
 		pHumidity.setAttribute("class", "card-text humidity")
-		pHumidity.textContent = "Humidity = " + weatherData[i].main.humidity + "%";
+		pHumidity.textContent = "Humidity: " + weatherData[i].main.humidity + "%";
 		
 		var pWindSpeed = document.createElement("p");
 		pWindSpeed.setAttribute("class", "card-text wind speed");
-		pWindSpeed.textContent = "Wind speed = " + weatherData[i].wind.speed + " MPH";
+		pWindSpeed.textContent = "Wind speed: " + weatherData[i].wind.speed + " MPH";
+
+		var imgEl = document.createElement("img")
+		imgEl.setAttribute("src", `http://openweathermap.org/img/wn/${weatherData[i].weather[0].icon}@2x.png`)
+		cardTitle.appendChild(imgEl);
 		
+		cardDiv.append(cardTitle)
 		cardDiv.append(pTemp)
 		cardDiv.append(pHumidity)
 		cardDiv.append(pWindSpeed);
-		
+
 		forecastContainer.append(cardDiv)
-		
-		renderIcons(weatherData[i].weather[0].icon)
+
 	}
-	// if(forecastContainer.style.display === "none") {
-	// 	forecastContainer.style.display = "block"
-	// } else {
-	// 	forecastContainer.style.display = "none";
-	// }
-
-	
-	
-}
-
-function renderIcons(iconCode) {
-	
-var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-$('.forecast').attr('src', iconURL);
 }
 
 init ();
-
-//w3 schools date object method to pass in unix timpestamp. Format out a date.
